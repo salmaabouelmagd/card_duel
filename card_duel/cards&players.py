b87a115bd276028e35fd7fba2318054beca7c6a2
salmaabouelmagd <210408920@st.biruni.edu.tr>
2024-05-26 20:10:29 +0300

    Update cards&players.py
    i added the q-learning class

--- card_duel/cards&players.py
+++ card_duel/cards&players.py
@@ -11,3 +11,27 @@ class Player:
         self.cards = []
         self.live_cards = []
         self.is_ai = is_ai
+
+class Qlearning:
+    def __init__(self):
+        self.q_table = {}
+        self.learning_rate = 0.1
+        self.discount = 0.9
+        self.epsilon = 0.1
+    
+    def actions(self, state, board):
+        empty_positions = [i for i, pos in enumerate(board) if pos is None]
+        if not empty_positions:
+            return random.choice(empty_positions), random.choice(random(len(state)))
+        else:
+            return max(self.q_table[state], key = self.q_table[state].get)
+    
+    def update_table(self, state, action, reward, next_state):
+        if state not in self.q_table:
+            self.q_table[state] = {}
+        if next_state not in self.q_table:
+            self.q_table[next_state] = {}
+        old_value = self.q_table[state].get(action, 0)
+        next_max = max(self.q_table[next_state].values()) if self.q_table[next_state] else 0
+        new_value = (1 - self.learning_rate) * old_value + self.learning_rate * (reward + self.discount * next_max)
+        self.q_table[state][action] = new_value
